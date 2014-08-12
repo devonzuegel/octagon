38caa0f1cc1df60f4312abf618cf956ae49a58db
Zack Elliott <hubridnoxx@gmail.com>
2014-08-11 17:15:35 -0700

    fixing up some js syntax and removing unused ids and classes

--- routes/privileges.js
+++ routes/privileges.js
@@ -2,8 +2,13 @@ exports.require_privileges = function require_privileges(req, res, include_msgs,
   if (req.isAuthenticated() && req.session.is_admin) {
     admin_fn(req, res);
   } else {
-    if (include_msgs)   req.flash('error', 'You do not have permission to perform that action.');
-    if (!req.isAuthenticated()) res.redirect('../login');
-    else    user_fn(req, res);
+    if (include_msgs) {
+      req.flash('error', 'You do not have permission to perform that action.');
+    }
+    if (!req.isAuthenticated()) {
+      res.redirect('../login');
+    } else {
+      user_fn(req, res);
+    }
   }
 }
