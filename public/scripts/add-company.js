5628e2f2b5b1afabe0e3425dbca52661982b9fe2
Zack Elliott <hubridnoxx@gmail.com>
2014-08-12 12:14:51 -0700

    added comments to add company object literal

--- public/scripts/add-company.js
+++ public/scripts/add-company.js
@@ -1,6 +1,10 @@
+// Object literal for handling all aspects of adding a company
 var addCompany = {
 
+  // Initialize object
   init: function() {
+
+    // Save elements references
     addCompany.elements = {
       openForm: $('#add-company-btn'),
       form: $('#add-company-form'),
@@ -12,32 +16,39 @@ var addCompany = {
       password2ErrorField: $('#password2-error div'),
     };
 
+    // Run on click 
     addCompany.elements.openForm.click(
-      addCompany.focus
+      addCompany.focusFirst
     );
 
+    // Run on focus
     addCompany.elements.form.submit(
       addCompany.validate
     );
   },
 
-  focus: function() {
+  // Focus the first input field 
+  focusFirst: function() {
     setTimeout(function(){
       addCompany.elements.usernameField.focus();
     }, 0);
   },
 
+  // Validate the form
   validate: function() {
+
+    // Store the error tests
     var noUsername = (addCompany.elements.usernameField.val().trim() === ''),
         noPassword = (addCompany.elements.passwordField.val().trim() === ''),
         differentPasswords = (addCompany.elements.passwordField.val() !== addCompany.elements.password2Field.val());
 
-
+    // Return the results of running the tests
     return addCompany.handleError(addCompany.elements.usernameErrorField, noUsername) &&
            addCompany.handleError(addCompany.elements.passwordErrorField, noPassword) &&
            addCompany.handleError(addCompany.elements.password2ErrorField, differentPasswords);
   },
 
+  // Given an error, handle the display of error fields
   handleError: function(error_field, error) {
     if(error) {
       error_field.show();
