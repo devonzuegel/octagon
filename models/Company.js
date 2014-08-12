b7ada39e06c05f0e4d8caa075642b030b6885a66
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-08-12 15:02:48 -0700

    Fixed add company bug (there were 2 username fields!)

--- models/Company.js
+++ models/Company.js
@@ -43,17 +43,22 @@ function obj_arr_to_str(p, obj) {
   }
 }
 
-function add(username, password, init_investmt_date, crunchbase_permalink, owners, callback) {
+function usd(num) {
+  return num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+function add(username, password, init_investmt_date, crunchbase_permalink, owners, cb) {
   if (crunchbase_permalink == '')     crunchbase_permalink = 'NO_PERMALINK_SELECTED';
   var permalink = '';
 
   api_mgr.get_cmpny(crunchbase_permalink, function(body) {
     // TODO what if p or p.data is undefined?
 
-    var p = JSON.parse(body).data; // parse data from crunchbase response
-    var profile = {}; // create empty profile to be saved into the new company
+    var p = JSON.parse(body).data;  // parse data from crunchbase response
+    var profile = {};  // create empty profile to be saved into the new company
 
-    if (p.response != false) {
+    // Crunchbase request returns successfully with complete profile
+    if (p.response) {
       permalink = crunchbase_permalink; // there is a valid crunchbase permalink
       profile = { 
         img_path:       (p.relationships.primary_image) ? "http://images.crunchbase.com/" + p.relationships.primary_image.items[0].path : undefined,
@@ -61,11 +66,14 @@ function add(username, password, init_investmt_date, crunchbase_permalink, owner
         description:    p.properties.description,
         homepage_url:   (p.properties.homepage_url) ? p.properties.homepage_url.replace('http://', '') : undefined,
         founded_on:     p.properties.founded_on,
-        total_funding:  (p.properties.total_funding_usd) ? p.properties.total_funding_usd.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') : undefined,
-        founders:       obj_arr_to_str(p, 'founders'), // build comma-separated string of founders' names
-        categories:     obj_arr_to_str(p, 'categories') // build comma-separated string of categories
+        total_funding:  (p.properties.total_funding_usd) ? usd(p.properties.total_funding_usd) : undefined,
+        founders:       obj_arr_to_str(p, 'founders'), // comma-separated string of founders' names
+        categories:     obj_arr_to_str(p, 'categories') // comma-separated string of categories
       };
-    } else { // with no crunchbase permalink, we have to make our own
+
+    // Crunchbase request returned empty
+    } else {
+      // with no crunchbase permalink, we have to make our own
       if (username)   permalink = username.replace(/\s+/g, '-').toLowerCase();
     }
 
@@ -81,60 +89,38 @@ function add(username, password, init_investmt_date, crunchbase_permalink, owner
     });
 
     c.save(function (err) {
-      if (err)  console.log('ERROR');
-      callback();
+      if (err)  return done(err);
+      cb();
     });
 
   });
 }
 
-// function update(u_param, orig_profile, updates, callback) {
-//   Companies.findOne({ username: u_param }, function (err, user) {
-//     if (err)  return done(err);
+/*
+function update(u_param, orig_profile, updates, callback) {
+  Companies.findOne({ username: u_param }, function (err, user) {
+    if (err)  return done(err);
 
-//     var profile = {};
-//     // populate profile with original user.profile
-//     for (var k in orig_profile)  profile[k] = orig_profile[k];
-//     // update the changes from the form
-//     for (var k in updates)      profile[k] = updates[k];
+    var profile = {};
+    // populate profile with original user.profile
+    for (var k in orig_profile)  profile[k] = orig_profile[k];
+    // update the changes from the form
+    for (var k in updates)      profile[k] = updates[k];
     
-//     callback(profile);
+    callback(profile);
 
-//     // save & redirect to updated profile
-//     user.save(function() { res.redirect('/portfolio/' + u_param); });
-//   });
-// }
-
+    // save & redirect to updated profile
+    user.save(function() { res.redirect('/portfolio/' + u_param); });
+  });
+}
+*/
 
 function salt_fn(pw) {
   console.log(pw + '----');
 }
 
-function find(username) {
-  Companies.findOne({ username: username, }, 
-                      function(err, user) {
-                        if (err)    return done(err);
-                        if (!user)  done(null, false);     
-                        return done(null, user);
-                      });
-
-}
-
-function has_privileges(req, admin_only){
-  if (!req.isAuthenticated()) return false;
-  if (!admin_only)  return true;
-  return req.session.is_admin;
-}
-
-function test(x) {
-  console.log(x);
-}
-
 module.exports.add = add;
 // module.exports.update = update;
-module.exports.test = test;
 module.exports.passport = passport;
 module.exports.CompanyDetails = Companies;
 module.exports.salt_fn = salt_fn;
-module.exports.has_privileges = has_privileges;
-// module.exports.username_inuse = username_inuse;
