63c45139ff0e7341b555ba05d2c6983c4aef3d1d
Zack Elliott <hubridnoxx@gmail.com>
2014-08-13 12:08:20 -0700

    added scroll handler and commented code

---
+++ public/scripts/Scroll.js
@@ -0,0 +1,40 @@
+// Object literal for handling changing elements on scroll
+var Scroll = {
+
+  // Initialize scroll object
+  init: function() {
+
+    // Store elements to be handled
+    this.elements = {
+      portfolioToolbar: $('.portfolio-toolbar'),
+      companyNavbar: $('.company-navbar')
+    };
+
+    // Store distances to be saved
+    this.distances = {
+      portfolioDist: 50,
+      companyDist: 300
+    };
+
+    // Position elements on page load
+    Scroll.positionElement(this.elements.portfolioToolbar, this.distances.portfolioDist);
+    Scroll.positionElement(this.elements.companyNavbar, this.distances.companyDist);
+
+    // On scroll, position elements
+    $(window).scroll(function() {
+      Scroll.positionElement(Scroll.elements.portfolioToolbar, Scroll.distances.portfolioDist);
+      Scroll.positionElement(Scroll.elements.companyNavbar, Scroll.distances.companyDist);
+    });
+  },
+
+  // Given an element and a distance, set the property of an object
+  positionElement: function(elem, dist) {
+
+    if(elem.length && $(window).scrollTop() >= dist) {
+      elem.addClass('top-fixed');
+    } else {
+      elem.removeClass('top-fixed');
+    }
+  }
+
+};
