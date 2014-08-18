8003715487e464798e212377a42f5eb47638c256
Zack Elliott <hubridnoxx@gmail.com>
2014-08-17 19:58:37 -0700

    added quarter calculation info

--- public/js/Company.js
+++ public/js/Company.js
@@ -1,24 +1,31 @@
 var Company = {
 
+  // Initializes the company
   init: function() {
 
+    // Stores important elements
     this.elements = {
       currentQuarter: $('#current-quarter'),
       daysRemaining: $('#days-remaining') 
     };
 
+    // Calls the fn that calculates the quarter statistics
     this.calcQuarter(this.elements.currentQuarter, this.elements.daysRemaining);
   },
 
+  // Calculates quarter time
   calcQuarter: function(e_current, e_days) {
 
-    var date = new Date(),
-        quarter = Math.floor(date.getMonth() / 3) + 1;
+    // Stores days remaining until start of next quarter
+    var daysRemaining = moment().startOf('quarter')
+                          .add(3, 'month')
+                          .diff(moment(), 'days');
 
-    if(quarter == 4) {
-      
-    }
+    // Stores the label suffix for days remaining
+    var daysLabel = (daysRemaining == 1) ? 'day' : 'days';
 
-    e_current.text('Q' + quarter + ' ' + date.getFullYear());
+    // Populates the elements with the correct values
+    e_current.text('Q' + moment().quarter() + ' ' + moment().year());
+    e_days.text(daysRemaining + ' ' + daysLabel);
   }
 };
