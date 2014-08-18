f82fe437f214a95a632e03746c25796f7a3ef20e
Zack Elliott <hubridnoxx@gmail.com>
2014-08-17 19:33:56 -0700

    installed moment and some small company section changes

--- public/js/Company.js
+++ public/js/Company.js
@@ -3,7 +3,22 @@ var Company = {
   init: function() {
 
     this.elements = {
+      currentQuarter: $('#current-quarter'),
+      daysRemaining: $('#days-remaining') 
+    };
+
+    this.calcQuarter(this.elements.currentQuarter, this.elements.daysRemaining);
+  },
+
+  calcQuarter: function(e_current, e_days) {
+
+    var date = new Date(),
+        quarter = Math.floor(date.getMonth() / 3) + 1;
+
+    if(quarter == 4) {
       
     }
+
+    e_current.text('Q' + quarter + ' ' + date.getFullYear());
   }
 };
