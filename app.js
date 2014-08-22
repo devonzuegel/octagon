b9157671f2a88cafd0d0250d1e1efbd3ac5fb95a
Zack Elliott <hubridnoxx@gmail.com>
2014-08-22 14:12:25 -0700

    added mongo/heroku conn to app

--- app.js
+++ app.js
@@ -47,6 +47,19 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+// Mongo/Heroku connection
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/test';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
