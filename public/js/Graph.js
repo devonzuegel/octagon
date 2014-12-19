33f081c8ab1be54f4de4f5067078bbf6fc1247c0
Devon Zuegel <devonzuegel@gmail.com>
2014-12-18 17:16:41 -0800

    Fixed "cash_data undefined" issue on non-companyprofile pages

--- public/js/Graph.js
+++ public/js/Graph.js
@@ -84,140 +84,148 @@ function generateGraph(el, data, width_multiplier) {
 
   return chart;
 }
-var cashGraph = generateGraph('#cashGraph', { 
-  xs: { 'Cash': 'x1' },
-  columns: [
-    convertData(cash_data, 'date', 'x1'),
-    convertData(cash_data, 'value', 'Cash'),
-  ],
-  type: 'area',
-  y: y_min_and_max(cash_data)
-}, 10/12);
+  
+// console.log(toString(typeof(cash_data)));
+// console.log('cash_data' in this);
+
+// if (toString(typeof(cash_data)) != undefined) {
+
+  var cashGraph = generateGraph('#cashGraph', { 
+    xs: { 'Cash': 'x1' },
+    columns: [
+      convertData(cash_data, 'date', 'x1'),
+      convertData(cash_data, 'value', 'Cash'),
+    ],
+    type: 'area',
+    y: y_min_and_max(cash_data)
+  }, 10/12);
 
 
-var grossBurnGraph = generateGraph('#grossBurnGraph', { 
-  xs: { 
-    'Forecasted Gross burn': 'x1', 
-    'Gross burn': 'x2' 
-  },
-  columns: [
-    convertData(pred_gross_burn_data, 'date', 'x1'),
-    convertData(gross_burn_data, 'date', 'x2'),
-    convertData(pred_gross_burn_data, 'value', 'Forecasted Gross burn'),
-    convertData(gross_burn_data, 'value', 'Gross burn')
-  ],
-  type: 'bar',
-  y: y_min_and_max(pred_gross_burn_data.concat(gross_burn_data))
-}, 8/12);
+  var grossBurnGraph = generateGraph('#grossBurnGraph', { 
+    xs: { 
+      'Forecasted Gross burn': 'x1', 
+      'Gross burn': 'x2' 
+    },
+    columns: [
+      convertData(pred_gross_burn_data, 'date', 'x1'),
+      convertData(gross_burn_data, 'date', 'x2'),
+      convertData(pred_gross_burn_data, 'value', 'Forecasted Gross burn'),
+      convertData(gross_burn_data, 'value', 'Gross burn')
+    ],
+    type: 'bar',
+    y: y_min_and_max(pred_gross_burn_data.concat(gross_burn_data))
+  }, 8/12);
 
-var netBurnGraph = generateGraph('#netBurnGraph', { 
-  xs: { 'Forecasted Net burn': 'x1', 'Net burn': 'x2' },
-  columns: [
-    convertData(pred_net_burn_data, 'date', 'x1'),
-    convertData(net_burn_data, 'date', 'x2'),
-    convertData(pred_net_burn_data, 'value', 'Forecasted Net burn'),
-    convertData(net_burn_data, 'value', 'Net burn')
-  ],
-  type: 'bar',
-  y: y_min_and_max(pred_net_burn_data.concat(net_burn_data))    
-}, 8/12);
+  var netBurnGraph = generateGraph('#netBurnGraph', { 
+    xs: { 'Forecasted Net burn': 'x1', 'Net burn': 'x2' },
+    columns: [
+      convertData(pred_net_burn_data, 'date', 'x1'),
+      convertData(net_burn_data, 'date', 'x2'),
+      convertData(pred_net_burn_data, 'value', 'Forecasted Net burn'),
+      convertData(net_burn_data, 'value', 'Net burn')
+    ],
+    type: 'bar',
+    y: y_min_and_max(pred_net_burn_data.concat(net_burn_data))    
+  }, 8/12);
 
-var revenueGraph = generateGraph('#revenueGraph', { 
-  xs: { 'Revenue': 'x1' },
-  columns: [
-    convertData(revenue_data, 'date', 'x1'),
-    convertData(revenue_data, 'value', 'Revenue')
-  ],
-  type: 'bar',
-  y: y_min_and_max(revenue_data)
-}, 0.75);
+  var revenueGraph = generateGraph('#revenueGraph', { 
+    xs: { 'Revenue': 'x1' },
+    columns: [
+      convertData(revenue_data, 'date', 'x1'),
+      convertData(revenue_data, 'value', 'Revenue')
+    ],
+    type: 'bar',
+    y: y_min_and_max(revenue_data)
+  }, 0.75);
 
-var headCountGraph = generateGraph('#headCountGraph', { 
-  xs: { 'Head count': 'x1' },
-  columns: [
-    convertData(head_count_data, 'date', 'x1'),
-    convertData(head_count_data, 'value', 'Head count')
-  ],
-  type: 'bar',
-  y: y_min_and_max(head_count_data)
-}, 0.75);
+  var headCountGraph = generateGraph('#headCountGraph', { 
+    xs: { 'Head count': 'x1' },
+    columns: [
+      convertData(head_count_data, 'date', 'x1'),
+      convertData(head_count_data, 'value', 'Head count')
+    ],
+    type: 'bar',
+    y: y_min_and_max(head_count_data)
+  }, 0.75);
 
-var avgDauGraph = generateGraph('#avgDauGraph', { 
-  xs: { 'AVG DAU': 'x1' },
-  columns: [
-    convertData(avg_dau_data, 'date', 'x1'),
-    convertData(avg_dau_data, 'value', 'AVG DAU')
-  ],
-  type: 'bar',
-  y: y_min_and_max(avg_dau_data)
-}, 0.75);
+  var avgDauGraph = generateGraph('#avgDauGraph', { 
+    xs: { 'AVG DAU': 'x1' },
+    columns: [
+      convertData(avg_dau_data, 'date', 'x1'),
+      convertData(avg_dau_data, 'value', 'AVG DAU')
+    ],
+    type: 'bar',
+    y: y_min_and_max(avg_dau_data)
+  }, 0.75);
 
-var avgMauGraph = generateGraph('#avgMauGraph', { 
-  xs: { 'AVG MAU': 'x1' },
-  columns: [
-    convertData(avg_mau_data, 'date', 'x1'),
-    convertData(avg_mau_data, 'value', 'AVG MAU')
-  ],
-  type: 'bar',
-  y: y_min_and_max(avg_mau_data)
-}, 0.75);
+  var avgMauGraph = generateGraph('#avgMauGraph', { 
+    xs: { 'AVG MAU': 'x1' },
+    columns: [
+      convertData(avg_mau_data, 'date', 'x1'),
+      convertData(avg_mau_data, 'value', 'AVG MAU')
+    ],
+    type: 'bar',
+    y: y_min_and_max(avg_mau_data)
+  }, 0.75);
 
-var churnGraph = generateGraph('#churnGraph', { 
-  xs: { 'Churn': 'x1' },
-  columns: [
-    convertData(churn_data, 'date', 'x1'),
-    convertData(churn_data, 'value', 'Churn')
-  ],
-  type: 'bar',
-  y: y_min_and_max(churn_data)
-}, 0.75);
+  var churnGraph = generateGraph('#churnGraph', { 
+    xs: { 'Churn': 'x1' },
+    columns: [
+      convertData(churn_data, 'date', 'x1'),
+      convertData(churn_data, 'value', 'Churn')
+    ],
+    type: 'bar',
+    y: y_min_and_max(churn_data)
+  }, 0.75);
 
-var ltvGraph = generateGraph('#ltvGraph', { 
-  xs: { 'LTV': 'x1' },
-  columns: [
-    convertData(ltv_data, 'date', 'x1'),
-    convertData(ltv_data, 'value', 'LTV')
-  ],
-  type: 'bar',
-  y: y_min_and_max(ltv_data)
-}, 0.75);
+  var ltvGraph = generateGraph('#ltvGraph', { 
+    xs: { 'LTV': 'x1' },
+    columns: [
+      convertData(ltv_data, 'date', 'x1'),
+      convertData(ltv_data, 'value', 'LTV')
+    ],
+    type: 'bar',
+    y: y_min_and_max(ltv_data)
+  }, 0.75);
 
-var lifetimeEstGraph = generateGraph('#lifetimeEstGraph', { 
-  xs: { 'Lifetime est.': 'x1' },
-  columns: [
-    convertData(lifetime_est_data, 'date', 'x1'),
-    convertData(lifetime_est_data, 'value', 'Lifetime est.')
-  ],
-  type: 'bar',
-  y: y_min_and_max(lifetime_est_data)
-}, 0.75);
+  var lifetimeEstGraph = generateGraph('#lifetimeEstGraph', { 
+    xs: { 'Lifetime est.': 'x1' },
+    columns: [
+      convertData(lifetime_est_data, 'date', 'x1'),
+      convertData(lifetime_est_data, 'value', 'Lifetime est.')
+    ],
+    type: 'bar',
+    y: y_min_and_max(lifetime_est_data)
+  }, 0.75);
 
-var cacGraph = generateGraph('#cacGraph', { 
-  xs: { 'CAC': 'x1' },
-  columns: [
-    convertData(cac_data, 'date', 'x1'),
-    convertData(cac_data, 'value', 'CAC')
-  ],
-  type: 'bar',
-  y: y_min_and_max(cac_data)
-}, 0.75);
+  var cacGraph = generateGraph('#cacGraph', { 
+    xs: { 'CAC': 'x1' },
+    columns: [
+      convertData(cac_data, 'date', 'x1'),
+      convertData(cac_data, 'value', 'CAC')
+    ],
+    type: 'bar',
+    y: y_min_and_max(cac_data)
+  }, 0.75);
 
-var aspGraph = generateGraph('#aspGraph', { 
-  xs: { 'ASP': 'x1' },
-  columns: [
-    convertData(asp_data, 'date', 'x1'),
-    convertData(asp_data, 'value', 'ASP')
-  ],
-  type: 'bar',
-  y: y_min_and_max(asp_data)
-}, 0.75);
+  var aspGraph = generateGraph('#aspGraph', { 
+    xs: { 'ASP': 'x1' },
+    columns: [
+      convertData(asp_data, 'date', 'x1'),
+      convertData(asp_data, 'value', 'ASP')
+    ],
+    type: 'bar',
+    y: y_min_and_max(asp_data)
+  }, 0.75);
 
-var gmPercentageGraph = generateGraph('#gmPercentageGraph', { 
-  xs: { 'GM %': 'x1' },
-  columns: [
-    convertData(gm_percentage_data, 'date', 'x1'),
-    convertData(gm_percentage_data, 'value', 'GM %')
-  ],
-  type: 'bar',
-  y: y_min_and_max(gm_percentage_data)
-}, 0.75);
+  var gmPercentageGraph = generateGraph('#gmPercentageGraph', { 
+    xs: { 'GM %': 'x1' },
+    columns: [
+      convertData(gm_percentage_data, 'date', 'x1'),
+      convertData(gm_percentage_data, 'value', 'GM %')
+    ],
+    type: 'bar',
+    y: y_min_and_max(gm_percentage_data)
+  }, 0.75);
+
+// }
